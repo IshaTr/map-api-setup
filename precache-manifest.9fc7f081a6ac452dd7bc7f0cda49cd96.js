7d68949e1924380a2857efdcc94e7fa6929ae048
Synup <synup@Synups-MacBook-Pro-2.local>
2019-11-12 02:29:20 +0530

    Updates

---
+++ precache-manifest.9fc7f081a6ac452dd7bc7f0cda49cd96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe23b5cd2363b68979bb6a7eaf7227a",
+    "url": "/IshaTr/map-api-setup.git/index.html"
+  },
+  {
+    "revision": "0c517753b3a9e0988066",
+    "url": "/IshaTr/map-api-setup.git/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f302b8420fdd37c441f1",
+    "url": "/IshaTr/map-api-setup.git/static/js/2.45b35a64.chunk.js"
+  },
+  {
+    "revision": "0c517753b3a9e0988066",
+    "url": "/IshaTr/map-api-setup.git/static/js/main.c93d96b0.chunk.js"
+  },
+  {
+    "revision": "ed9aa5a466cea46c84ee",
+    "url": "/IshaTr/map-api-setup.git/static/js/runtime-main.aecd9d3a.js"
+  }
+]);
